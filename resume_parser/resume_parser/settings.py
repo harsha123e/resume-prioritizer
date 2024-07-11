c8d1703d959f0e047a638e48387f62ee00658bef
Harsha <nsriharsha3@gmail.com>
2024-07-11 19:18:50 +0530

    Add configuration for vercel

--- resume_parser/resume_parser/settings.py
+++ resume_parser/resume_parser/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ji2x45v5g3#kp#9+fh5g*1!h+u5jxo&aetm1z21_6f*%kv&g9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -122,6 +123,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- resume_parser/resume_parser/urls.py
+++ resume_parser/resume_parser/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index_view, name='index'),
     path('upload/', views.upload_view, name='upload_view'),
     path('load-demo-data/', views.load_demo_data_view, name='load_demo_data'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
