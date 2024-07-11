05d552714dddf3a5ec70e1b6a18ae46e253a5c85
Harsha <nsriharsha3@gmail.com>
2024-07-11 20:07:05 +0530

    Update config and test run locally

--- resume_parser/resume_parser/urls.py
+++ resume_parser/resume_parser/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +8,3 @@ urlpatterns = [
     path('upload/', views.upload_view, name='upload_view'),
     path('load-demo-data/', views.load_demo_data_view, name='load_demo_data'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
