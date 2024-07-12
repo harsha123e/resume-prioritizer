de21644d1b96c7e95e1a0730e67fa47559261fdc
Harsha <nsriharsha3@gmail.com>
2024-07-12 13:40:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+# Create and activate a virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
